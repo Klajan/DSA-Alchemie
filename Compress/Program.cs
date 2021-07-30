5f708addc96f29110271360c8dbebdd3adfd84c1
Klajan <48261494+Klajan@users.noreply.github.com>
2021-07-30 14:19:55 +0200

    Updated Compress to check hash before overwriting existing file

--- Compress/Program.cs
+++ Compress/Program.cs
@@ -1,10 +1,28 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace Compress
 {
     internal static class Program
     {
+        private static SHA256 Sha256 = SHA256.Create();
+
+        private static bool CompareArray<T>(ref T[] array1, ref T[] array2)
+        {
+            bool isSame = true;
+            if(array1.Length == array2.Length)
+            {
+                for(int i = 0; i < array1.Length; i++)
+                {
+                    isSame &= array1[i].Equals(array2[i]);
+                }
+            }
+            return isSame;
+        }
+
         private static void WriteDeflate(string pathIN, string pathOUT)
         {
             using FileStream streamIN = new(pathIN, FileMode.Open);
@@ -13,13 +31,34 @@ namespace Compress
             streamIN.CopyTo(deflate);
         }
 
+        private static async Task<bool> CompareHash(string pathFile, string pathArchive)
+        {
+            
+            using FileStream stream1 = new(pathFile, FileMode.Open);
+            using FileStream stream2 = new(pathArchive, FileMode.Open);
+            using DeflateStream deflate = new(stream2, CompressionMode.Decompress);
+            var hashTask1 = Sha256.ComputeHashAsync(stream1);
+            var hashTask2 = Sha256.ComputeHashAsync(deflate);
+            var hash1 = await hashTask1;
+            var hash2 = await hashTask2;
+            return CompareArray<byte>(ref hash1, ref hash2);
+        }
+
         private static void Main(string[] args)
         {
             if (args.Length != 0)
             {
                 if (File.Exists(args[0]))
                 {
-                    WriteDeflate(args[0], args[0] + ".deflate");
+                    bool isSame = false;
+                    if(File.Exists(args[0] + ".deflate"))
+                    {
+                        var task = CompareHash(args[0], args[0] + ".deflate");
+                        task.Wait();
+                        isSame = task.Result;
+                        Console.WriteLine($"File {args[0]} and {args[0]}.deflate skipped, SHA256 is equal!");
+                    }
+                    if (!isSame) WriteDeflate(args[0], args[0] + ".deflate");
                 }
             }
         }
